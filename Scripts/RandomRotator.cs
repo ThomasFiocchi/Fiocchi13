114a29eb0e62512b96b993cc2dd86b15c32f2402
ThomasFiocchi <42783203+ThomasFiocchi@users.noreply.github.com>
2018-12-14 16:46:08 -0600

    Add files via upload

---
+++ Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿//RandomRotator.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+	public float tumble;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		// gives a random 3D vector for the velocity for the Asteroid
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
